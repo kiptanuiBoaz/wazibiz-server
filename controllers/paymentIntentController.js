2f0c525ec0e29ab7034a9fd093a074f1fcc76e7f
Boaz <boaserem022@gmail.com>
2023-07-21 11:19:20 +0300

    changed currency to kes

--- controllers/paymentIntentController.js
+++ controllers/paymentIntentController.js
@@ -8,7 +8,7 @@ const stripePay = async (req, res) => {
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
         amount: calculateOrderAmount(items),
-        currency: "usd",
+        currency: "kes",
         automatic_payment_methods: {
             enabled: true,
         },
--- controllers/postStkController.js
+++ controllers/postStkController.js
@@ -6,7 +6,7 @@ const postStk = async (req, res) => {
     const shortCode = 174379;
     const phone = req.body.phone.substring(1);
     //calculate the amount using libary
-    const amount = Math.floor(calculateOrderAmount(req.body.items) / 140);
+    const amount = Math.floor(calculateOrderAmount(req.body.items));
     const passkey = process.env.passkey;
 
     const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
