5d5f9bef0337762828bf9e365071d5abbae40927
Boaz <boaserem022@gmail.com>
2023-07-24 16:06:08 +0300

    reverted /result method

--- middleware/createToken.js
+++ middleware/createToken.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-// ePKu93UtTzsqaEYG4hr5UwiEKcqwC07iRJBty7BZRFI2WhYibvQFavf9dm2/atabKat/5k8oDkk10hgxnMyfQhWs8inTuLU31Pb9BHkNtV4DzTV7PTSy4BQYXwnzFqdob9q0MWteLOW6Vwb2oWgqMr1PyBQcIJao0mFm9QrYgE8vamqcuwNcPnw/X2PrTZqknc0CBiF1Ki06Lrw2GD+tiJS8GKR2VIkdeZ9re3lrDu3Nshxu1CasnojOgnN9zEtbyM8i/EGKAIZvXAMMamx2TEAti1F3HkhhGyy2vw1Im0KsBNcDXNvmwhjOx8R6vIV/8fnA5+Demau4pttS5LnfSg==
+
 const createToken = async (req, res, next) => {
     const secret = process.env.secret;
     const consumer = process.env.consumer;
@@ -9,7 +9,7 @@ const createToken = async (req, res, next) => {
             "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
             {
                 headers: {
-                    authorization: `Basic ${auth}`,
+                    Authorization: `Basic ${auth}`,
                 },
             }
         )
